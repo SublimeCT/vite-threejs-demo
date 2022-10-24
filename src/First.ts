78f30101d9e276e73c0078c5b8e1bdb75e0883b7
kuidi <hellosc@qq.com>
2022-10-24 16:40:44 +0800

    feat: 增加 First.vue

--- src/First.ts
+++ src/First.ts
@@ -26,9 +26,14 @@ export default class First {
 	}
 
 	initScene() {
+    // 创建场景对象
 		this.scene = new THREE.Scene();
-    const geometry = new THREE.BoxGeometry(100, 100, 100)
+    // 创建球体几何对象
+    // const geometry = new THREE.SphereGeometry(60, 40, 40)
+    // 创建立方体几何对象
+    const geometry = new THREE.BoxGeometry(50, 100, 100)
     const material = new THREE.MeshLambertMaterial({ color: 0x0000FF })
+    // 材质对象
     const mesh = new THREE.Mesh(geometry, material)
     this.scene.add(mesh)
 
@@ -38,23 +43,26 @@ export default class First {
     this.scene.add(point)
 
     // 环境光
-    const ambient = new THREE.AmbientLight(0x444444)
+    const ambient = new THREE.AmbientLight(0xFF4444)
     this.scene.add(ambient)
 
+    // 相机设置
     const width = window.innerWidth
     const height = window.innerHeight
     const k = width / height
-    const s = 200
+    const s = 200 // 三维场景显示范围控制系数, 系数越大, 显示的范围越大
 
+    // 相机对象
     const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000)
     camera.position.set(200, 300, 200)
-    camera.lookAt(this.scene.position)
+    camera.lookAt(this.scene.position) // 设置相机方向
 
+    // 渲染器对象
     this.renderer = new THREE.WebGLRenderer()
-    this.renderer.setSize(width, height)
-    this.renderer.setClearColor(0xb9d3ff, 1)
+    this.renderer.setSize(width, height) // 设置渲染区域尺寸
+    this.renderer.setClearColor(0xb9d3ff, 1) // 设置背景色
     document.body.appendChild(this.renderer.domElement)
-    this.renderer.render(this.scene, camera)
+    this.renderer.render(this.scene, camera) // 指定渲染操作(场景和相机)
 	}
 
 }
