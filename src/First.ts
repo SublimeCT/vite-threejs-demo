5a34db3568f00be9b4de81e356d02a0e109cb3da
kuidi <hellosc@qq.com>
2022-10-27 10:39:36 +0800

    feat: 基于 requestAnimationFrame 实现旋转动画

--- src/First.ts
+++ src/First.ts
@@ -5,7 +5,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 export default class First {
 	private renderer!: THREE.WebGLRenderer;
 	private scene!: THREE.Scene;
-	private camera!: THREE.PerspectiveCamera;
+	private camera!: THREE.OrthographicCamera;
+  private mesh!: THREE.Mesh
 
 	private lightAmbient!: THREE.AmbientLight;
 	private lightPoint!: THREE.PointLight;
@@ -34,8 +35,8 @@ export default class First {
     const geometry = new THREE.BoxGeometry(50, 100, 100)
     const material = new THREE.MeshLambertMaterial({ color: 0x0000FF })
     // 材质对象
-    const mesh = new THREE.Mesh(geometry, material)
-    this.scene.add(mesh)
+    this.mesh = new THREE.Mesh(geometry, material)
+    this.scene.add(this.mesh)
 
     // 点光源
     const point = new THREE.PointLight(0xFFFFFF)
@@ -56,13 +57,20 @@ export default class First {
     const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000)
     camera.position.set(200, 300, 200)
     camera.lookAt(this.scene.position) // 设置相机方向
+    this.camera = camera
 
     // 渲染器对象
     this.renderer = new THREE.WebGLRenderer()
     this.renderer.setSize(width, height) // 设置渲染区域尺寸
     this.renderer.setClearColor(0xb9d3ff, 1) // 设置背景色
     document.body.appendChild(this.renderer.domElement)
-    this.renderer.render(this.scene, camera) // 指定渲染操作(场景和相机)
+    // this.renderer.render(this.scene, this.camera) // 指定渲染操作(场景和相机)
+    this.render()
 	}
 
+  render() {
+    this.renderer.render(this.scene, this.camera)
+    this.mesh.rotateY(0.01)
+    requestAnimationFrame(this.render.bind(this))
+  }
 }
